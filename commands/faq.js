1401ea82c678720da005e164194024aa7580629e
SmokeyStack <seansyahril04@gmail.com>
2023-12-25 22:07:57 -0500

    Added yaml parser

--- commands/faq.js
+++ commands/faq.js
@@ -1,11 +1,39 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const yaml = require('js-yaml');
 
-const exampleEmbed = new EmbedBuilder()
+const faq = new Map();
+const foldersPath = path.join(__dirname, '../entries');
+const commandFiles = fs
+    .readdirSync(foldersPath)
+    .filter((file) => file.endsWith('.yaml'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(foldersPath, file);
+    const doc = yaml.load(fs.readFileSync(filePath, 'utf8'));
+    const faqEntry = new EmbedBuilder()
+        .setColor(0x0099ff)
+        .setTitle(doc['title'])
+        .setDescription(doc['body'])
+        .setTimestamp();
+    faq.set(doc['name'], faqEntry);
+}
+
+const infoEmbed = new EmbedBuilder()
     .setColor(0x0099ff)
     .setTitle('Info')
     .setDescription(
         'This bot was created by [SmokeyStack](https://smokeystack.dev) for the purpose of making a FAQ bot for the Bedrock Add-Ons Discord Server.'
     )
+    .addFields({
+        name: 'Managing Entries',
+        value: 'To manage entries, please make a pull request on GitHub.'
+    })
+    .addFields({
+        name: 'Source Code',
+        value: '[GitHub]()'
+    })
     .setTimestamp();
 
 module.exports = {
@@ -13,21 +41,36 @@ module.exports = {
         .setName('faq')
         .setDescription('Commands for accessing the FAQ')
         .addSubcommand((subcommand) =>
-            subcommand.setName('get').setDescription('Return the FAQ Entry')
-        )
-        .addSubcommand((subcommand) =>
             subcommand
-                .setName('list')
-                .setDescription('List all the FAQ Entries')
+                .setName('get')
+                .setDescription('Return the FAQ Entry')
+                .addStringOption((option) =>
+                    option
+                        .setName('name')
+                        .setDescription('The name of the FAQ Entry')
+                        .setRequired(true)
+                        .setAutocomplete(true)
+                )
         )
         .addSubcommand((subcommand) =>
             subcommand.setName('info').setDescription('Info about the FAQ Bot')
         ),
     async execute(interaction) {
-        if (interaction.options.getSubcommand() === 'info') {
-            await interaction.reply({ embeds: [exampleEmbed] });
-        } else {
-            await interaction.reply('Huh?');
-        }
+        if (interaction.options.getSubcommand() === 'info')
+            await interaction.reply({ embeds: [infoEmbed] });
+        else if (interaction.options.getSubcommand() === 'get')
+            await interaction.reply({
+                embeds: [faq.get(interaction.options.getString('name'))]
+            });
+    },
+    async autocomplete(interaction) {
+        const focusedValue = interaction.options.getFocused();
+        const choices = [...faq.keys()];
+        const filtered = choices.filter((choice) =>
+            choice.startsWith(focusedValue)
+        );
+        await interaction.respond(
+            filtered.map((choice) => ({ name: choice, value: choice }))
+        );
     }
 };
--- deploy.js
+++ deploy.js
@@ -3,8 +3,9 @@ const { clientId, token } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
 
+console.log('Deploying commands...');
+
 const commands = [];
-// Grab all the command folders from the commands directory you created earlier
 const foldersPath = path.join(__dirname, 'commands');
 const commandFiles = fs
     .readdirSync(foldersPath)
@@ -23,30 +24,22 @@ for (const file of commandFiles) {
     }
 }
 
-// Construct and prepare an instance of the REST module
 const rest = new REST().setToken(token);
-
 rest.put(Routes.applicationCommands(clientId), { body: [] })
     .then(() => console.log('Successfully deleted all application commands.'))
     .catch(console.error);
-
-// and deploy your commands!
 (async () => {
     try {
         console.log(
             `Started refreshing ${commands.length} application (/) commands.`
         );
-
-        // The put method is used to fully refresh all commands in the guild with the current set
         const data = await rest.put(Routes.applicationCommands(clientId), {
             body: commands
         });
-
         console.log(
             `Successfully reloaded ${data.length} application (/) commands.`
         );
     } catch (error) {
-        // And of course, make sure you catch and log any errors!
         console.error(error);
     }
 })();
